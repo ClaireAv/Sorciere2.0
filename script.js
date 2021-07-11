ecf9947f4142953593153f35f2a282acd50805c5
Claire <avenel.cl@gmail.com>
2021-07-11 13:52:34 +0200

    CAROUSEL OK

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 let header = document.getElementById("haut");
 header.addEventListener ('mouseover', ()=>{header.classList.toggle('active');});
 
+// CAROUSEL
+$('.slider').slick({
+    arrows: false,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
+ 
+          
